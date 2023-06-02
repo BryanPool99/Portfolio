32270dcee54fe95af7817539426dcb491d280879
Bryan <bryanpool991@hotmail.com>
2023-06-02 14:11:42 -0500

    add component basic header

---
+++ src/components/Header/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact me</li>
+        </ul>
+    </nav>
+  )
+}
