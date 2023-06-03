62666345b71169dfc6c2456ce4382cc805691370
Bryan <bryanpool991@hotmail.com>
2023-06-02 19:15:16 -0500

    componetn de icons a usar

---
+++ src/assets/Img/Icons/icons.js
@@ -0,0 +1,7 @@
+import { AiOutlineLinkedin, AiOutlineGithub, AiOutlineCodepen } from "react-icons/ai";
+import { SiFrontendmentor } from "react-icons/si";
+
+export const LinkedinIcon = AiOutlineLinkedin;
+export const GithubIcon = AiOutlineGithub;
+export const CodepenIcon = AiOutlineCodepen;
+export const FrontendmentorIcon = SiFrontendmentor;
