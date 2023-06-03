71acd234e6db1f07b2c7fedd944b121192f92547
Bryan <bryanpool991@hotmail.com>
2023-06-02 19:16:36 -0500

    component main incompleto

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { LinkedinIcon, GithubIcon } from "./../../assets/Img/Icons/icons";
+import perfilImage from "./../../assets/Img/perfil.png";
 
 export default function Main() {
   return (
-    <main>Este es el main</main>
+    <main>
+      <div className='titular'>
+        <p>Hola a todos mi nombre es</p>
+        <h1> Bryan Arredondo</h1>
+        <h2>Web Developer</h2>
+      </div>
+      <div className='social_media'>
+        <button><LinkedinIcon/></button>
+        <button><GithubIcon/></button>
+      </div>
+      <figure className='img'>
+        <img src={perfilImage} alt="Foto de perfil de usuario" />
+      </figure>
+    </main>
   )
 }
