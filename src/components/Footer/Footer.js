694a4c56451181ea0f410fea76df07863156e7eb
Bryan <bryanpool991@hotmail.com>
2023-06-02 19:15:45 -0500

    component footer incompleto

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { LinkedinIcon, GithubIcon, CodepenIcon, FrontendmentorIcon } from "./../../assets/Img/Icons/icons";
 
 export default function Footer() {
   return (
-    <Footer>este es el footer</Footer>
+    <footer>
+      <div>
+        <a href='https://www.linkedin.com/in/bryan-pool-arredondo-ovalle/'><LinkedinIcon/></a>
+        <a href='https://github.com/BryanPool99'><GithubIcon/></a>
+        <a href='https://www.frontendmentor.io/profile/BryanPool99'><FrontendmentorIcon/></a>
+        <a href='https://codepen.io/bryanpool99'><CodepenIcon/></a>
+      </div>
+      <div>
+        <p>
+          <b>&copy; Bryan Arredondo</b> Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
   )
 }
