5afae9732722e33c32d17f41fae78c58350a0507
Bryan <bryanpool991@hotmail.com>
2023-06-02 19:16:12 -0500

    uso de link de react router

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav>
         <ul>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Projects</li>
-            <li>Contact me</li>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
         </ul>
     </nav>
+  
   )
 }
