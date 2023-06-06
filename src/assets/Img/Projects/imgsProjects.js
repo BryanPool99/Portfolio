8be403bbcd4b5864959b5f9d140bf0e09f599899
Bryan <bryanpool991@hotmail.com>
2023-06-06 12:31:05 -0500

    agregado de imagenes de proyectos subdividos en carpetas

---
+++ src/assets/Img/Projects/imgsProjects.js
@@ -0,0 +1,31 @@
+export const images = {
+    "html-css": [
+      { src: require("./HtmlCSS/Product preview card component.jpg"), alt: "Product preview card component.jpg",
+        description:"",
+        linkDemo:"",linkCode:""
+      },
+      { src: require("./HtmlCSS/QR code component.jpg"), alt: "QR code component",
+        description:"",
+        linkDemo:"",linkCode:""
+      },
+      { src: require("./HtmlCSS/Testimonials grid section.jpg"),alt:"Testimonials grid section",
+        description:"",
+        linkDemo:"",linkCode:""
+      }
+      // Agrega más imágenes de HTML & CSS
+    ],
+   "react": [
+      { src: require("./React/Social media dashboard with theme switcher.jpg"), alt: "Social media dashboard with theme switcher",
+        description:"",
+        linkDemo:"",linkCode:""
+      },
+      { src: require("./React/chat-panel.png"), alt: "chat-panel",
+        description:"",
+        linkDemo:"",linkCode:""
+      },
+      // Agrega más imágenes de React
+    ],
+    // Agrega más categorías y sus imágenes correspondientes
+    
+  };
+  
