ef5eee7426853aade3d03698c18d362d1b987c10
Bryan <bryanpool991@hotmail.com>
2023-06-06 12:30:38 -0500

    agregado de imagenes

---
+++ src/assets/Img/Skills/imgsSkills.js
@@ -0,0 +1,23 @@
+import bootstrap from './bootstrap.png'
+import css3 from './css3.png';
+import html5 from './html5.png';
+import git from './git.png';
+import java from './java.png';
+import js from './js.png';
+import mysql from './mysql.png';
+import nodeJS from './nodeJS.png';
+import react from './react.png';
+import sass from './sass.png';
+import spring from './spring.png';
+
+export const BootstrapImg=bootstrap;
+export const Css3Img = css3;
+export const Html5Img = html5;
+export const GitImg = git;
+export const JavaImg = java;
+export const JsImg = js;
+export const MysqlImge=mysql;
+export const NodeJSImg=nodeJS;
+export const ReactImg = react;
+export const SassImg = sass;
+export const SpringImg = spring;
