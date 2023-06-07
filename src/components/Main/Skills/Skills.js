e36f7a704af50bde8e3c132ef6a60e27616afd9b
Bryan <bryanpool991@hotmail.com>
2023-06-07 13:21:02 -0500

    component skills  mejorado

--- src/components/Main/Skills/Skills.js
+++ src/components/Main/Skills/Skills.js
@@ -1,57 +1,14 @@
 import React from 'react';
-import { BootstrapImg, Css3Img, GitImg, Html5Img, JavaImg, JsImg, MysqlImge, NodeJSImg, ReactImg, SassImg, SpringImg } from "./../../../assets/Img/Skills/imgsSkills";
+import Skill from './component_skill/skill';
 export default function Skills() {
   return (
     <section>
       <h2>SKILLS</h2>
-      <aside>
-          <h3>USANDO AHORA:</h3>
-          <div>
-            <figure>
-              <img src={Html5Img} alt="logo-html5" />
-            </figure>
-            <figure>
-              <img src={Css3Img} alt="logo-css3" />
-            </figure>
-            <figure>
-              <img src={BootstrapImg} alt="" />
-            </figure>
-            <figure>
-              <img src={JsImg} alt="logo-js" />
-            </figure>
-            <figure>
-              <img src={SassImg} alt="logo-sass" />
-            </figure>
-            <figure>
-              <img src={ReactImg} alt="logo-react" />
-            </figure>
-            <figure>
-              <img src={GitImg} alt="logo-git" />
-            </figure>
-          </div>
-      </aside>
-      <aside>
-        <h3>Aprendiendo:</h3>
-        <div>
-          <figure>
-            <img src={NodeJSImg} alt="logo-nodejs" />
-          </figure>
-          <figure>
-            <img src={SpringImg} alt="logo-spring" />
-          </figure>
-          <figure>
-            <img src={MysqlImge} alt="logo-mysql" />
-          </figure>
-        </div>
-      </aside>
-      <aside>
-        <h3>Otros Skills:</h3>
-        <div>
-          <figure>
-            <img src={JavaImg} alt="logo-java" />
-          </figure>
-        </div>
-      </aside>
+      <Skill title="Lenguaje de programacion" category="lenguaje"/>
+      <Skill title="Frontend Development" category="front"/>
+      <Skill title="Backend Development" category="back"/>
+      <Skill title="Database" category="db"/>
+      <Skill title="Tools" category="tools"/>
     </section>
   )
 }
