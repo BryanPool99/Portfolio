eeeac7d49b5eb43473ed8347b0b166a308119b35
Bryan <bryanpool991@hotmail.com>
2023-06-07 13:17:22 -0500

    add all categorias

--- src/assets/Img/Projects/imgsProjects.js
+++ src/assets/Img/Projects/imgsProjects.js
@@ -1,31 +1,65 @@
 export const images = {
+    "api": [
+      { src: require("./API/Advice generator app.jpg"), alt: "advice generator app",
+        description:"Aplicación generadora de consejos",
+        linkDemo:"https://tiny-genie-52480a.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/advice-generator-app-main"
+      },
+      { src: require("./API/REST Countries API with color theme switcher.jpg"), alt: "Country API",
+        description:"API de países REST con conmutador de tema de color",
+        linkDemo:"https://bespoke-semifreddo-bee05a.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/REST%20Countries%20API%20with%20color%20theme%20switcher"
+      },
+    ],
     "html-css": [
       { src: require("./HtmlCSS/Product preview card component.jpg"), alt: "Product preview card component.jpg",
-        description:"",
-        linkDemo:"",linkCode:""
+        description:"Componente de la tarjeta de vista previa del producto",
+        linkDemo:"https://heartfelt-kitten-dace9f.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/Product%20preview%20card%20component"
       },
       { src: require("./HtmlCSS/QR code component.jpg"), alt: "QR code component",
-        description:"",
-        linkDemo:"",linkCode:""
+        description:"Componente de código QR",
+        linkDemo:"https://leafy-semifreddo-7bce05.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/QR%20code%20component"
       },
       { src: require("./HtmlCSS/Testimonials grid section.jpg"),alt:"Testimonials grid section",
-        description:"",
-        linkDemo:"",linkCode:""
+        description:"Sección de cuadrícula de testimonios",
+        linkDemo:"https://effortless-piroshki-d1eaa2.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/Testimonials%20grid%20section"
       }
-      // Agrega más imágenes de HTML & CSS
+    ],
+    "js": [
+      { src: require("./JS/Interactive rating component.jpg"), alt: "interactive rating component",
+        description:"Componente de calificación interactiva",
+        linkDemo:"https://idyllic-lamington-37e7ff.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/Interactive%20rating%20component"
+      },
+      { src: require("./JS/Intro component with sign-up form.jpg"), alt: "sign-up form",
+        description:"Componente de introducción con formulario de registro",
+        linkDemo:"https://frolicking-horse-cb98de.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/Intro%20component%20with%20sign-up%20form"
+      },
+      { src: require("./JS/Time tracking dashboard.jpg"), alt: "time tracking dashboard",
+      description:"Panel de seguimiento de tiempo",
+      linkDemo:"https://timely-cat-b47fed.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/time-tracking-dashboard-main"
+      },
+      { src: require("./JS/Tip calculator app.jpg"), alt: "Tip calculator app",
+        description:"Aplicación de calculadora de punta",
+        linkDemo:"https://celadon-faun-70dcf6.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/Tip%20calculator%20app"
+      }
     ],
    "react": [
       { src: require("./React/Social media dashboard with theme switcher.jpg"), alt: "Social media dashboard with theme switcher",
-        description:"",
-        linkDemo:"",linkCode:""
+        description:"Panel de redes sociales con conmutador de temas",
+        linkDemo:"https://lambent-alfajores-6f4277.netlify.app/",linkCode:"https://github.com/BryanPool99/Social-media-dashboard-with-theme-switcher/tree/master"
       },
       { src: require("./React/chat-panel.png"), alt: "chat-panel",
-        description:"",
-        linkDemo:"",linkCode:""
+        description:"Componente de panel de chat",
+        linkDemo:"https://chat-panel.vercel.app/",linkCode:"https://github.com/BryanPool99/Reto-UIDesignDaily/tree/master/chatp-panel"
       },
-      // Agrega más imágenes de React
     ],
-    // Agrega más categorías y sus imágenes correspondientes
-    
+    "sass": [
+      { src: require("./Sass/News homepage.jpg"), alt: "news homepage",
+        description:"Página de inicio de noticias",
+        linkDemo:"https://celadon-moonbeam-f9daad.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/News%20homepage"
+      },
+      { src: require("./Sass/Pricing component with toggle.jpg"), alt: "pricing component with toogle",
+        description:"Componente de precios con palanca",
+        linkDemo:"https://taupe-shortbread-1c933a.netlify.app/",linkCode:"https://github.com/BryanPool99/Retos-FrontendMentor/tree/master/Pricing%20component%20with%20toggle"
+      },
+    ]
   };
   
