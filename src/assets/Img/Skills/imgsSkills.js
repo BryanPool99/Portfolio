ec4d4ff5b96efc6ec66cec111c35fa090ab5a4ad
Bryan <bryanpool991@hotmail.com>
2023-06-07 13:18:07 -0500

    data img skills

--- src/assets/Img/Skills/imgsSkills.js
+++ src/assets/Img/Skills/imgsSkills.js
@@ -1,23 +1,67 @@
-import bootstrap from './bootstrap.png'
-import css3 from './css3.png';
-import html5 from './html5.png';
-import git from './git.png';
-import java from './java.png';
-import js from './js.png';
-import mysql from './mysql.png';
-import nodeJS from './nodeJS.png';
-import react from './react.png';
-import sass from './sass.png';
-import spring from './spring.png';
-
-export const BootstrapImg=bootstrap;
-export const Css3Img = css3;
-export const Html5Img = html5;
-export const GitImg = git;
-export const JavaImg = java;
-export const JsImg = js;
-export const MysqlImge=mysql;
-export const NodeJSImg=nodeJS;
-export const ReactImg = react;
-export const SassImg = sass;
-export const SpringImg = spring;
+export const images = {
+    "back": [
+      { 
+        src: require("./Backend/nodeJS.png"), alt: "nodeJS"
+      },
+      { 
+        src: require("./Backend/springBoot.png"), alt: "spring-boot"
+      }
+    ],
+   "db": [
+      { 
+        src: require("./DataBase/mysql.png"), alt: "mysql"
+      },
+      { 
+        src: require("./DataBase/sqlServer.png"), alt: "sqlServer"
+      }
+    ],
+    "front": [
+        { 
+            src: require("./Frontend/bootstrap.png"), alt: "bootstrap"
+        },
+        {  
+            src: require("./Frontend/css3.png"), alt: "css3"
+        },
+        { 
+            src: require("./Frontend/html5.png"), alt: "html5"
+        },
+        {  
+            src: require("./Frontend/react.png"), alt: "react"
+        },
+        { 
+            src: require("./Frontend/sass.png"), alt: "sass"
+        }
+      ],
+    "lenguaje": [
+        { 
+            src: require("./LenguajeDeProgramacion/java.png"), alt: "java"
+        },
+        { 
+            src: require("./LenguajeDeProgramacion/js.png"), alt: "js"
+        },
+      ],
+    "tools": [
+        { 
+            src: require("./Tools/git.png"), alt: "git"
+        },
+        { 
+            src: require("./Tools/insomnia.png"), alt: "imsonia"
+        },
+        { 
+            src: require("./Tools/intellij-idea.png"), alt: "intellij"
+        },
+        { 
+            src: require("./Tools/netbeans.png"), alt: "netbeans"
+        },
+        { 
+            src: require("./Tools/npm.png"), alt: "npm"
+        },
+        { 
+            src: require("./Tools/postman.png"), alt: "postman"
+        },
+        { 
+            src: require("./Tools/vscode.png"), alt: "vscode"
+        }
+    ]
+  };
+  
