052eec6fc7948bc28d30b452e10a7a8facd462d5
Bryan <bryanpool991@hotmail.com>
2023-06-07 13:19:53 -0500

    component about mejorado

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <h2>SOBRE MI</h2>
       <div>
         <p>
-        Soy estudiante de la universidad UTP, actualmente curso el 9no ciclo de la carrera de Ing. De Sistemas e informática. Tengo experiencia en el maquetado de páginas webs haciendo uso de tecnologías web como HTML, CSS, SASS, JS y React.js. Asimismo en la parte de servidor uso el lenguaje de programación JAVA con el framework Spring(Básico) y en la parte de base de datos uso el MySQL.
+        Soy estudiante de la universidad UTP, actualmente curso el 9no ciclo de la carrera de Ing. De Sistemas e informática. Tengo experiencia en el maquetado de páginas webs haciendo uso de tecnologías web como HTML, CSS, SASS, JS y React.js. Asimismo en la parte de servidor uso el lenguaje de programación JAVA con el framework SpringBoot(Básico) y en la parte de base de datos uso el MySQL.
         </p>
         <p>
         Esta experiencia adquirida diariamente, me ha permitido obtener diversos conocimientos no solo técnicos sino también blandos como por ejemplo la organización, creatividad, pensamiento lógico-crítico, mucha aptitud de investigación, innovación, trabajo en equipo y disposición para el cambio.
