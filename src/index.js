e40e842809ed755b55c47d5a8ed4a3c38091edd5
Bryan <bryanpool991@hotmail.com>
2023-06-20 12:19:58 -0500

    index js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/Scss/styles.scss'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './assets/Scss/styles.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
