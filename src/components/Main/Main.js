982dfb77f8502626b8863b6128122ce631b8af46
Bryan <bryanpool991@hotmail.com>
2023-06-21 11:21:03 -0500

    component main finalizado

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,17 +4,17 @@ import perfilImage from "./../../assets/Img/perfil.png";
 
 export default function Main() {
   return (
-    <main>
-      <aside className='titular'>
-        <p>Hola a todos mi nombre es</p>
+    <main className='d-flex align-items-center justify-content-evenly flex-wrap'>
+      <aside>
+        <b>Hola a todos mi nombre es</b>
         <h1> Bryan Arredondo</h1>
         <h2>Web Developer</h2>
+        <div className='social_media d-flex'>
+          <a href='https://www.linkedin.com/in/bryan-pool-arredondo-ovalle/'><LinkedinIcon/></a>
+          <a href='https://github.com/BryanPool99'><GithubIcon/></a>
+        </div>
       </aside>
-      <aside className='social_media'>
-        <button><LinkedinIcon/></button>
-        <button><GithubIcon/></button>
-      </aside>
-      <figure className='img'>
+      <figure>
         <img src={perfilImage} alt="Foto de perfil de usuario" />
       </figure>
     </main>
