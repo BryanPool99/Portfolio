7b21c9853ac0beab9acb1ea0fe821e95fc668cf0
Bryan <bryanpool991@hotmail.com>
2023-06-22 10:54:21 -0500

    component skill

--- src/components/Main/Skills/Skills.js
+++ src/components/Main/Skills/Skills.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Skill from './component_skill/skill';
 export default function Skills() {
   return (
-    <section>
-      <h2>SKILLS</h2>
-      <Skill title="Lenguaje de programacion" category="lenguaje"/>
-      <Skill title="Frontend Development" category="front"/>
-      <Skill title="Backend Development" category="back"/>
-      <Skill title="Database" category="db"/>
-      <Skill title="Tools" category="tools"/>
+    <section className='skills'>
+      <div className='container-skills container-fluid d-flex flex-column align-items-center justify-content-center'>
+        <h2>SKILLS</h2>
+        <Skill title="Lenguaje de programacion" category="lenguaje"/>
+        <Skill title="Frontend Development" category="front"/>
+        <Skill title="Backend Development" category="back"/>
+        <Skill title="Database" category="db"/>
+        <Skill title="Tools" category="tools"/>
+      </div>
     </section>
   )
 }
