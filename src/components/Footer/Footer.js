2ba91b64578728c4a88e7553dccc0e49f75c3d5f
Bryan <bryanpool991@hotmail.com>
2023-06-22 10:52:47 -0500

    component footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import { LinkedinIcon, GithubIcon, CodepenIcon, FrontendmentorIcon } from "./../
 
 export default function Footer() {
   return (
-    <footer className='bg-black d-flex flex-column align-items-center'>
-      <div>
+    <footer className='d-flex flex-column align-items-center justify-content-center'>
+      <div className='d-flex flex-row align-items-center justify-content-center flex-wrap'>
         <a  className='text-light m-2' href='https://www.linkedin.com/in/bryan-pool-arredondo-ovalle/'><LinkedinIcon/></a>
         <a  className='text-light m-2' href='https://github.com/BryanPool99'><GithubIcon/></a>
         <a  className='text-light m-2' href='https://www.frontendmentor.io/profile/BryanPool99'><FrontendmentorIcon/></a>
