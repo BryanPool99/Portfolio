409aaab41483d288517c493f617afded883f8d8e
Bryan <bryanpool991@hotmail.com>
2023-12-28 10:01:29 -0500

    mejora de footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import { LinkedinIcon, GithubIcon, CodepenIcon, FrontendmentorIcon } from "./../
 
 export default function Footer() {
   return (
-    <footer className='d-flex flex-column align-items-center justify-content-center'>
+    <footer className='d-flex flex-column align-items-center justify-content-end'>
       <div className='d-flex flex-row align-items-center justify-content-center flex-wrap'>
         <a  className='text-light m-2' href='https://www.linkedin.com/in/bryan-pool-arredondo-ovalle/'><LinkedinIcon/></a>
         <a  className='text-light m-2' href='https://github.com/BryanPool99'><GithubIcon/></a>
